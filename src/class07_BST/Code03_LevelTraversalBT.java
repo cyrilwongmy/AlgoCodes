5699d8200b41285f63c87afd91051473329c928d
scymw2 <scymw2@nottingham.edu.cn>
2021-02-15 22:54:08 +0800

    层序遍历；层的最多节点

--- src/class07_BST/Code03_LevelTraversalBT.java
+++ src/class07_BST/Code03_LevelTraversalBT.java
@@ -1,8 +1,53 @@
 package class07_BST;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author mingyan wang
  * @date 2021/2/14 7:35 PM
  */
 public class Code03_LevelTraversalBT {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int v) {
+            value = v;
+        }
+    }
+
+    public static void level(Node head) {
+        if (head == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            // remove from head of queue
+            Node node = queue.poll();
+            System.out.println(node.value);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        level(head);
+        System.out.println("========");
+    }
+
 }
