86e9f6bb38d8aaf683d66fb2938e5152da60179e
scymw2 <scymw2@nottingham.edu.cn>
2021-03-01 11:31:16 +0800

    贪心算法：点灯，最省切铜板

--- src/class09_GreedyAlgo/Code03_LessMoneySplitGold.java
+++ src/class09_GreedyAlgo/Code03_LessMoneySplitGold.java
@@ -1,8 +1,88 @@
 package class09_GreedyAlgo;
 
+import java.util.PriorityQueue;
+
 /**
+ * 一块金条切成两半，是需要花费和长度数值一样的铜板的。
+ * 比如长度为20的金条，不管怎么切，都要花费20个铜板。
+ * 一群人想整分整块金条，怎么分最省铜板?
+ * 例如,给定数组{10,20,30}，代表一共三个人，
+ * 整块金条长度为60，金条要分成10，20，30三个部分。
  * @author mingyan wang
  * @date 2021/2/27 7:07 PM
  */
 public class Code03_LessMoneySplitGold {
+    public static int lessMoney1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        return process(arr, 0);
+    }
+    public static int process(int[] arr, int pre) {
+        if (arr.length == 1) {
+            return pre;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                ans = Math.min(ans, process(copyAndMergeTwo(arr, i, j), pre + arr[i] + arr[j]));
+            }
+        }
+        return ans;
+    }
+
+    public static int[] copyAndMergeTwo(int[] arr, int i, int j) {
+        int[] ans = new int[arr.length - 1];
+        int ansi = 0;
+        for (int arri = 0; arri < arr.length; arri++) {
+            if (arri != i && arri != j) {
+                ans[ansi++] = arr[arri];
+            }
+        }
+        ans[ansi] = arr[i] + arr[j];
+        return ans;
+    }
+
+    public static int lessMoney2(int[] arr) {
+        // Java 默认小根堆
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int j : arr) {
+            priorityQueue.add(j);
+        }
+        // 10, 20, 30
+        int totalMoney = 0;
+        int cur = 0;
+        // 一定要是计算后的结果再进入优先队列
+        while (priorityQueue.size() > 1) {
+            cur = priorityQueue.poll() + priorityQueue.poll();
+            totalMoney += cur;
+            priorityQueue.add(cur);
+        }
+
+
+        return totalMoney;
+    }
+
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 100000;
+        int maxSize = 6;
+        int maxValue = 1000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            if (lessMoney1(arr) != lessMoney2(arr)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
 }
