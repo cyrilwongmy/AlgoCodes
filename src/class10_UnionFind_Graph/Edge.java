e198acff77306e3a1bb91026da824e1fbd14c3fb
scymw2 <scymw2@nottingham.edu.cn>
2021-03-04 18:45:09 +0800

    图的深度优先遍历和宽度优先遍历

--- src/class10_UnionFind_Graph/Edge.java
+++ src/class10_UnionFind_Graph/Edge.java
@@ -5,4 +5,13 @@ package class10_UnionFind_Graph;
  * @date 2021/3/2 12:28 PM
  */
 public class Edge {
+    public int weight;
+    public Node from;
+    public Node to;
+
+    public Edge(int weight, Node from, Node to) {
+        this.weight = weight;
+        this.from = from;
+        this.to = to;
+    }
 }
--- src/class10_UnionFind_Graph/Node.java
+++ src/class10_UnionFind_Graph/Node.java
@@ -1,8 +1,24 @@
 package class10_UnionFind_Graph;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * @author mingyan wang
  * @date 2021/3/2 12:28 PM
  */
 public class Node {
+    public int value;
+    public int in;
+    public int out;
+    public ArrayList<Node> nexts;
+    public ArrayList<Edge> edges;
+
+    public Node(int value) {
+        this.value = value;
+        this.in = 0;
+        this.out = 0;
+        this.nexts = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
 }
