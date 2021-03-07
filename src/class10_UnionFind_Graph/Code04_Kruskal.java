34eba53d8c994c82c415cabc3a3ad4b77282fc88
scymw2 <scymw2@nottingham.edu.cn>
2021-03-07 10:47:46 +0800

    图的拓扑排序和PK算法

--- src/class10_UnionFind_Graph/Code04_Kruskal.java
+++ src/class10_UnionFind_Graph/Code04_Kruskal.java
@@ -1,8 +1,101 @@
 package class10_UnionFind_Graph;
 
+import java.util.*;
+
 /**
  * @author mingyan wang
  * @date 2021/3/2 12:27 PM
  */
 public class Code04_Kruskal {
+    // Union-Find Set
+    public static class UnionFind {
+        // key 某一个节点， value key节点往上的节点
+        private HashMap<Node, Node> fatherMap;
+        // key 某一个集合的代表节点, value key所在集合的节点个数。方便进行小挂大操作
+        private HashMap<Node, Integer> sizeMap;
+
+        public UnionFind() {
+            fatherMap = new HashMap<>();
+            sizeMap = new HashMap<>();
+        }
+
+        public void makeSets(Collection<Node> nodes) {
+            fatherMap.clear();
+            sizeMap.clear();
+            for (Node node : nodes) {
+                fatherMap.put(node, node);
+                sizeMap.put(node, 1);
+            }
+        }
+
+        private Node findFather(Node n) {
+            Stack<Node> path = new Stack<>();
+            while (fatherMap.get(n) != n) {
+                path.add(n);
+                n = fatherMap.get(n);
+            }
+            while (!path.isEmpty()) {
+                Node node = path.pop();
+                fatherMap.put(node, n);
+            }
+            return n;
+        }
+
+        public boolean isSameSet(Node a, Node b) {
+            return findFather(a) == findFather(b);
+        }
+
+        public void union(Node a, Node b) {
+            if (a == null || b == null) {
+                return;
+            }
+            Node aFather = findFather(a);
+            Node bFather = findFather(b);
+            if (aFather != bFather) {
+                Integer aSize = sizeMap.get(aFather);
+                Integer bSize = sizeMap.get(bFather);
+                Node bigSet = aSize > bSize ? aFather : bFather;
+                Node smallSet = aSize > bSize ? bFather : aFather;
+                fatherMap.put(smallSet, bigSet);
+                sizeMap.remove(smallSet);
+                sizeMap.put(bigSet, aSize + bSize);
+            }
+
+        }
+    }
+
+
+    public static class EdgeComparator implements Comparator<Edge> {
+
+        @Override
+        public int compare(Edge o1, Edge o2) {
+            return o1.weight - o2.weight;
+        }
+
+    }
+
+    /**
+     * 步骤：
+     * 1. 建立一个heap保存边，最小权重先被取出来
+     * 2. 判断是否两边的点都在一个set，不在的话加入同一个set。这条边加入result set.
+     * @param graph
+     * @return
+     */
+    public static Set<Edge> kruskalMST(Graph graph) {
+        UnionFind unionFind = new UnionFind();
+        unionFind.makeSets(graph.nodes.values());
+        PriorityQueue<Edge> minEdgeHeap = new PriorityQueue<>();
+        for (Edge edge : graph.edges) {
+            minEdgeHeap.offer(edge);
+        }
+        HashSet<Edge> resultSet = new HashSet<>();
+        while (!minEdgeHeap.isEmpty()) {
+            Edge edge = minEdgeHeap.poll();
+            if (!unionFind.isSameSet(edge.from, edge.to)) {
+                resultSet.add(edge);
+                unionFind.union(edge.from, edge.to);
+            }
+        }
+        return resultSet;
+    }
 }
--- src/class10_UnionFind_Graph/Code05_Prim.java
+++ src/class10_UnionFind_Graph/Code05_Prim.java
@@ -1,8 +1,58 @@
 package class10_UnionFind_Graph;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
+ * 只限于使用无向图，有向图需要处理。
+ * 算法步骤：
+ * 1. 任意选择graph的点集中的一个点
+ * 2. 解锁该点的所有边，加入到minHeap
+ * 3. 从minHeap选择一条边，如果边的两头都在解锁的点set，那么放弃这条边。
+ *                      如果没有在，那么把新的一个点加入set，并且把点的nexts全部加入minHeap
+ * 4. 循环之前3. 操作
  * @author mingyan wang
  * @date 2021/3/2 12:28 PM
  */
 public class Code05_Prim {
+    /**
+     * 给定一个graph返回最小生成图的所有边的集合
+     * @param graph
+     * @return
+     */
+    public static Set<Edge> primMST(Graph graph) {
+        // assert graph != null，图上有节点
+        HashSet<Node> unlockedNodeSet = new HashSet<>();
+        PriorityQueue<Edge> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
+        HashSet<Edge> resultSet = new HashSet<>();
+        for (Node node : graph.nodes.values()) {
+            if (!unlockedNodeSet.contains(node)) {
+                unlockedNodeSet.add(node);
+                for (Edge edge : node.edges) {
+                    minHeap.offer(edge);
+                }
+                while (!minHeap.isEmpty()) {
+                    Edge edge = minHeap.poll();
+                    Node toNode = edge.to;
+                    if (!unlockedNodeSet.contains(toNode)) {
+                        unlockedNodeSet.add(toNode);
+                        resultSet.add(edge);
+                        for (Edge toNodeEdge : toNode.edges) {
+                            minHeap.offer(toNodeEdge);
+                        }
+                    }
+                }
+            }
+        }
+        return resultSet;
+    }
+
+    // 请保证graph是连通图
+    // graph[i][j]表示点i到点j的距离，如果是系统最大值代表无路
+    // 返回值是最小连通图的路径之和
+    public static int prim(int[][] graph) {
+        return 0;
+    }
 }
