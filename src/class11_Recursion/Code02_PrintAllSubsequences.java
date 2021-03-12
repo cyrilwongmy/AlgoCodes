633118c90ede77fc97aaa178023163985ffae83e
scymw2 <scymw2@nottingham.edu.cn>
2021-03-12 21:44:34 +0800

    字符串，数字转字符串递归问题

--- src/class11_Recursion/Code02_PrintAllSubsequences.java
+++ src/class11_Recursion/Code02_PrintAllSubsequences.java
@@ -1,8 +1,88 @@
 package class11_Recursion;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author mingyan wang
  * @date 2021/3/8 11:16 AM
  */
 public class Code02_PrintAllSubsequences {
+
+    /**
+     * 打印一个字符串的全部子序列，可以有重复的子序列。
+     * （注意不是字串，字串是不能跳跃字符选择，面试要问清楚）
+     * abc --> a, ab, abc, b, bc, c
+     * 算法流程：从左往右，经过每一个char时候选择是否pick这个char
+     * @param s
+     * @return
+     */
+    public static List<String> subs(String s) {
+        char[] charArray = s.toCharArray();
+        List<String> ans = new ArrayList<>();
+        process1(charArray, 0, "", ans);
+
+        return ans;
+    }
+
+    public static void process1(char[] charArray, int index, String path ,List<String> ans) {
+        // base case
+        if (index == charArray.length) {
+            ans.add(path);
+            return;
+        }
+        // choose next char
+        process1(charArray, index + 1, path + charArray[index], ans);
+        // don't choose next char
+        process1(charArray, index + 1, path, ans);
+    }
+
+    /**
+     * 打印一个字符串的全部子序列，要求不要出现重复字面值的子序列
+     * @param s
+     * @return
+     */
+    public static List<String> subsNoRepeat(String s) {
+        if (s == null || "".equals(s)) {
+            return new ArrayList<>();
+        }
+        char[] charArray = s.toCharArray();
+        HashSet<String> ans = new HashSet<>();
+        process2(charArray, 0, "", ans);
+
+        return new ArrayList<>(ans);
+    }
+
+    public static void process2(char[] charArray, int index, String path, HashSet<String> ans) {
+        if (index == charArray.length) {
+            ans.add(path);
+            return;
+        }
+        // choose char in index
+        process2(charArray, index + 1, path + charArray[index], ans);
+        // don't choose
+        process2(charArray, index + 1, path , ans);
+
+    }
+
+
+
+        public static void main(String[] args) {
+        String test = "aacc";
+//        String test = "abcd";
+        List<String> ans1 = subs(test);
+        List<String> ans2 = subsNoRepeat(test);
+
+        for (String str : ans1) {
+            System.out.println(str);
+        }
+        System.out.println("=================");
+        for (String str : ans2) {
+            System.out.println(str);
+        }
+        System.out.println("=================");
+
+    }
 }
--- src/class11_Recursion/Code05_ConvertToLetterString.java
+++ src/class11_Recursion/Code05_ConvertToLetterString.java
@@ -1,8 +1,98 @@
 package class11_Recursion;
 
 /**
+ * 规定1和A对应、2和B对应、3和C对应...
+ * 那么一个数字字符串比如"111”就可以转化为:
+ * "AAA"、"KA"和"AK"
+ * 给定一个只有数字字符组成的字符串str，
+ * 返回有多少种转化结果
  * @author mingyan wang
  * @date 2021/3/8 11:17 AM
  */
 public class Code05_ConvertToLetterString {
+    public static int number(String str) {
+        if (str == null || "".equals(str)) {
+            return 0;
+        }
+        int[] ans = new int[1];
+        process(str.toCharArray(), 0, ans);
+        return ans[0];
+    }
+
+    /**
+     * 110011
+     * @param str
+     * @param index
+     * @param ans
+     */
+    private static void process(char[] str, int index, int[] ans) {
+        if (index == str.length) {
+            ans[0] = ans[0] + 1;
+            return;
+        }
+        if ('0' == (str[index])) {
+            return;
+        }
+        if ('1' == (str[index])) {
+            // only take it as 1.
+            process(str, index + 1, ans);
+            // take it as 10, 11...19
+            if (index + 1 < str.length) {
+                process(str, index + 2, ans);
+            }
+            return;
+        }
+        if ('2' == (str[index])) {
+            // only take it as 2
+            process(str, index + 1, ans);
+            // take it as 20 ... 26
+            if (index + 1 < str.length && str[index] >= '0' && str[index] <= '6') {
+                process(str, index + 2, ans);
+            }
+            // otherwise not a result
+            return;
+        }
+        return;
+    }
+
+
+
+
+    // i之前的位置，如何转化已经做过决定了, 不用再关心
+    // i... 有多少种转化的结果
+    // 实现方法二，更加简介一些
+    public static int process2(char[] str, int i) {
+        if (i == str.length) { // base case
+            return 1;
+        }
+        // i没有到终止位置, 比如10，只能转成J，不能转成A0
+        if (str[i] == '0') {
+            return 0;
+        }
+        // str[i]字符不是‘0’
+        if (str[i] == '1') {
+            int res = process2(str, i + 1); // i自己作为单独的部分，后续有多少种方法
+            if (i + 1 < str.length) {
+                res += process2(str, i + 2); // (i和i+1)作为单独的部分，后续有多少种方法
+            }
+            return res;
+        }
+        if (str[i] == '2') {
+            int res = process2(str, i + 1); // i自己作为单独的部分，后续有多少种方法
+            // (i和i+1)作为单独的部分并且没有超过26，后续有多少种方法
+            if (i + 1 < str.length && (str[i + 1] >= '0' && str[i + 1] <= '6')) {
+                res += process2(str, i + 2); // (i和i+1)作为单独的部分，后续有多少种方法
+            }
+            return res;
+        }
+        // str[i] == '3' ~ '9'
+        // 只有一种选择，所以不需要考虑 递归考虑
+        return process2(str, i + 1);
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(number("11111"));
+    }
 }
