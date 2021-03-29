6f015cf250a338fc52007dc329834248957111a3
scymw2 <scymw2@nottingham.edu.cn>
2021-03-29 09:36:43 +0800

    单调栈

--- src/SecondBootCamp/class01_SlideWindow/Code03_MonotonousStack.java
+++ src/SecondBootCamp/class01_SlideWindow/Code03_MonotonousStack.java
@@ -1,8 +1,179 @@
 package SecondBootCamp.class01_SlideWindow;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * @author mingyan wang
  * @date 2021/3/24 4:53 PM
  */
 public class Code03_MonotonousStack {
+    /**
+     * 无重复值的单调栈
+     * @param arr
+     * @return
+     */
+    public static int[][] getNearLessNoRepeat(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new int[0][0];
+        }
+        int[][] res = new int[arr.length][2];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                int popIndex = stack.pop();
+                int leftLessIndex = stack.isEmpty() ? -1 : stack.peek();
+                // poped left min
+                res[popIndex][0] = leftLessIndex;
+                // poped right min
+                res[popIndex][1] = i;
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            res[pop][0] = stack.isEmpty() ? -1 : stack.peek();
+            res[pop][1] = -1;
+        }
+        return res;
+    }
+
+    /**
+     * 有重复值的单调栈
+     * @param arr
+     * @return 矩阵表示左右最近最小值
+     * 	[
+     * 	  0: [-1, 1]
+     * 	  1: [-1, 2]
+     * 	]
+     */
+    public static int[][] getNearLess(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new int[0][0];
+        }
+        int[][] res = new int[arr.length][2];
+        Stack<ArrayList<Integer>> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek().get(0)] > arr[i]) {
+                ArrayList<Integer> pop = stack.pop();
+                for (Integer popIndex : pop) {
+                    res[popIndex][0] = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+                    res[popIndex][1] = i;
+                }
+            }
+            // 相等的、比你小的
+            if (!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]) {
+                stack.peek().add(i);
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                stack.push(list);
+            }
+        }
+        while (!stack.isEmpty()) {
+            ArrayList<Integer> pop = stack.pop();
+            int leftIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+            for (Integer popIndex : pop) {
+                res[popIndex][0] = leftIndex;
+                res[popIndex][1] = -1;
+            }
+        }
+        return res;
+    }
+
+
+
+    // for test
+    public static int[] getRandomArrayNoRepeat(int size) {
+        int[] arr = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int swapIndex = (int) (Math.random() * arr.length);
+            int tmp = arr[swapIndex];
+            arr[swapIndex] = arr[i];
+            arr[i] = tmp;
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] getRandomArray(int size, int max) {
+        int[] arr = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * max) - (int) (Math.random() * max);
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[][] rightWay(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        for (int i = 0; i < arr.length; i++) {
+            int leftLessIndex = -1;
+            int rightLessIndex = -1;
+            int cur = i - 1;
+            while (cur >= 0) {
+                if (arr[cur] < arr[i]) {
+                    leftLessIndex = cur;
+                    break;
+                }
+                cur--;
+            }
+            cur = i + 1;
+            while (cur < arr.length) {
+                if (arr[cur] < arr[i]) {
+                    rightLessIndex = cur;
+                    break;
+                }
+                cur++;
+            }
+            res[i][0] = leftLessIndex;
+            res[i][1] = rightLessIndex;
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[][] res1, int[][] res2) {
+        if (res1.length != res2.length) {
+            return false;
+        }
+        for (int i = 0; i < res1.length; i++) {
+            if (res1[i][0] != res2[i][0] || res1[i][1] != res2[i][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int max = 20;
+        int testTimes = 2000000;
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr1 = getRandomArrayNoRepeat(size);
+            int[] arr2 = getRandomArray(size, max);
+            if (!isEqual(getNearLessNoRepeat(arr1), rightWay(arr1))) {
+                System.out.println("Oops!");
+                printArray(arr1);
+                break;
+            }
+            if (!isEqual(getNearLess(arr2), rightWay(arr2))) {
+                System.out.println("Oops!");
+                printArray(arr2);
+                break;
+            }
+        }
+    }
 }
--- src/SecondBootCamp/class01_SlideWindow/Code04_AllTimesMinToMax.java
+++ src/SecondBootCamp/class01_SlideWindow/Code04_AllTimesMinToMax.java
@@ -1,8 +1,83 @@
 package SecondBootCamp.class01_SlideWindow;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * @author mingyan wang
  * @date 2021/3/24 4:53 PM
  */
 public class Code04_AllTimesMinToMax {
+
+    /**
+     * 暴力方法
+     * @param arr
+     * @return
+     */
+    public static int max1(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int minNum = Integer.MAX_VALUE;
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                    minNum = Math.min(minNum, arr[k]);
+                }
+                max = Math.max(max, minNum * sum);
+            }
+        }
+        return max;
+    }
+
+    public static int max2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int[] prefixSum = new int[arr.length];
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefixSum[i] += (prefixSum[i - 1] + arr[i]);
+        }
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                Integer popIndex = stack.pop();
+                int curResult = arr[popIndex] *
+                        (stack.isEmpty() ? prefixSum[i - 1] :
+                        (prefixSum[i - 1] - prefixSum[stack.peek()]));
+                max = Math.max(max, curResult);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            int curResult = arr[pop] *
+                    (stack.isEmpty() ? prefixSum[arr.length - 1] : prefixSum[arr.length - 1] - prefixSum[stack.peek()]);
+            max = Math.max(max, curResult);
+        }
+        return max;
+    }
+
+    public static int[] gerenareRondomArray() {
+        int[] arr = new int[(int) (Math.random() * 20) + 10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 101);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTimes = 2000000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr = gerenareRondomArray();
+            if (max1(arr) != max2(arr)) {
+                System.out.println("FUCK!");
+                break;
+            }
+        }
+        System.out.println("test finish");
+    }
 }
